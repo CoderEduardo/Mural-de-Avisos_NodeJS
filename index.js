15cf0c886ac6bc604f99084a37af58ad2c6fa677
Luis Eduardo Ferreira de Faria <luiseduardo250305@gmail.com>
2023-06-19 20:16:46 -0300

    Estrutura do css pronta

--- index.js
+++ index.js
@@ -1,6 +1,23 @@
 const express = require("express")
 const app = express()
+const handlebars = require("express-handlebars")
 const PORTA = 3000
+
+app.use(express.static("public"))
+
+//Configurando o handlebars para ser o template
+app.engine("handlebars",handlebars.engine({
+    defaultLayout:"main",
+    runtimeOptions:{
+        allowProtoPropertiesByDefault:true,
+        allowProtoMethodsByDefault:true
+    }
+}))
+app.set("view engine","handlebars")
+
+app.get("/",(req,res)=>{
+    res.render("home")
+})
 
 app.listen(PORTA,()=>{
     console.log(`O servidor está rodando na porta: ${PORTA}`)
